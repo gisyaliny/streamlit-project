2d2ff9ad707dfcd870cad72dd9232210f21a41f3
gisyaliny <690521906@qq.com>
2024-03-26 21:48:03 -0500

    fix heatmap prob

--- apps/heatmap.py
+++ apps/heatmap.py
@@ -8,7 +8,7 @@ def app():
 
     m = leafmap.Map()
     m.add_basemap("Esri.WorldTopoMap")
-    m.add_heatmap_demo()
+    # m.add_heatmap_demo()
     m.add_scatter_plot_demo()
     
     m.to_streamlit(height=700)
