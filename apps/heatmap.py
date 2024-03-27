af69287adc24577859aaef4490f7856aa56f485f
gisyaliny <690521906@qq.com>
2024-03-26 21:47:20 -0500

    fix heatmap

--- apps/heatmap.py
+++ apps/heatmap.py
@@ -6,14 +6,9 @@ def app():
 
     st.title("Heatmap")
 
-    m = leafmap.split_map(
-    left_layer="NLCD 2001 CONUS Land Cover",
-    right_layer="NLCD 2016 CONUS Land Cover",
-    left_label="2001",
-    right_label="2016",
-    label_position="bottom",
-    center=[36.1, -114.9],
-    zoom=10,
-    )
+    m = leafmap.Map()
+    m.add_basemap("Esri.WorldTopoMap")
+    m.add_heatmap_demo()
+    m.add_scatter_plot_demo()
     
     m.to_streamlit(height=700)
